8934697b3c1964adae5e8cc1df9adf832e18a0c7
adam.lai[賴奕牧] <adam.lai@104.com.tw>
2022-10-27 19:00:42 +0800

    [工程大學] 回家作業 webpack 設定

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./main.css"
+import "./color.scss"
 import { hello, monsters, date } from "./components.js"
 
 // commonjs
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require("path")
 const htmlWebpackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-// const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const ImageminPlugin = require('imagemin-webpack-plugin').default
+
 
 module.exports = {
     entry: {
@@ -22,6 +23,25 @@ module.exports = {
             {
                 test: /\.(png|gif|jpg)$/,
                 use: ["file-loader"]
+            },
+            {
+              test: /\.s[ac]ss$/i,
+              use: [
+                MiniCssExtractPlugin.loader,
+                {
+                  loader: 'css-loader',
+                  options: {
+                    sourceMap: true,
+                  },
+                },
+                {
+                  loader: 'sass-loader',
+                  options: {
+                    implementation: require('sass'),
+                    sourceMap: true,
+                  },
+                },
+              ],
             }
         ]
     },
@@ -37,7 +57,10 @@ module.exports = {
             template: "./src/index.html"
         }),
         // new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin()
+        new MiniCssExtractPlugin(),
+        new ImageminPlugin({
+          test: /\.(jpe?g|png|gif|svg)$/i
+        })
     ],
     devtool: "inline-source-map",
     optimization: {
